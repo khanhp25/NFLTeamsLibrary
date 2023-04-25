375f01c1b62615caaff84c3e34e2eae550ffe9c6
khanhp25 <116367383+khanhp25@users.noreply.github.com>
2023-04-25 19:56:00 -0400

    Update NFL Teams.js

--- NFL Teams.js
+++ NFL Teams.js
@@ -32,13 +32,13 @@ var city = getColumn(url, 4);
 var stadium = getColumn(url, 5);
 var capacity = getColumn(url, 6);
 var headCoach = getColumn(url, 7);
-//Heach coaches in each division
 
 
-//Finding the head coaches in each team
-//teams- a list contained with all the football team
-//coach - a new list created by me for putting the result
-//The function will return the list coach for the result
+/*
+Takes an NFL team as a parameter and uses it to print out the head coach of the entered team
+@param team(string) - the user's desired NFL team
+@return coach(string) - returns the coach of the entered NFL team
+*/
 function findCoach(team){
   var coach = [];
   for (var i in teams){
@@ -52,15 +52,18 @@ function findCoach(team){
   return coach;
 }
 
-console.log(findCoach("Miami Dolphins"));
-
-
-//finding the stadium from the division he put in. 
-//matches - list self created, will contain the matched stadium
-//divisons - a list contained with divisions 
-//will return matches of the matche stadium of each division
-//conferences - a list containing all the conferences
-//stadiums - a list contained with all the stdiums  
+console.log(findCoach("New Orleans Pelicans"));
+console.log(findCoach("buffalo bills"));
+console.log(findCoach("new england"))
+console.log(findCoach("giants"));
+console.log(findCoach("baltimore"));
+console.log(findCoach("cinci"));
+console.log(findCoach("Browns"));
+/*
+Uses an NFL division as a parameter and returns all of the stadiums in the entered division
+@param division(string) - the user's desired NFL division
+@return matches(list) - returns a list of all of the stadiums in the entered division
+*/
 function getStadiumsInDivision(division){
   var matches = [];
   for (var i in divisions){
@@ -69,52 +72,60 @@ function getStadiumsInDivision(division){
     }
   }
   if (matches.length == 0){
-    matches.push("NO NHL teams are in the division" + " " + division);
+    matches.push("NO NFL stadiums are in the division" + " " + division);
   }
   return matches;
 }
-console.log(getStadiumsInDivision("NFC East"));
+console.log(getStadiumsInDivision("Afc east"));
+console.log(getStadiumsInDivision("afc west"));
+console.log(getStadiumsInDivision("north"));
+console.log(getStadiumsInDivision("afc"));
+console.log(getStadiumsInDivision("NFC"));
+console.log(getStadiumsInDivision("West"));
+console.log(getStadiumsInDivision("Atlantic"));
 
 
-//Biggest arena in each division
-// max - biggest value it can go
-// maxIndex - make the index start a 0
-// divisions - a list contained with all the divions. 
-// capacity - a list with all the capacity of each stadium 
-//conferences - a list containing all the conferences
-//returns the highest capacity of the stadiums
+/*
+Uses an NFL division as a parameter and returns the stadium in the division with the highest capacity
+@param division(string) - the user's desired NFL division
+@return stadium[maxIndex](string) - returns the stadium with the largest capacity in the entered division
+*/
 
 function findMaxCapactiy(division){
   max = 10000000;
   maxIndex = 0;
   for(var i in divisions){
-    if(conferences[i].toLowerCase() + " " + divisions[i].toLowerCase() == division.toLowerCase() && parseFloat(capacity[i]) < max){
+    if((conferences[i].toLowerCase() + " " + divisions[i].toLowerCase()).includes(division.toLowerCase()) && parseFloat(capacity[i]) < max){
       max = parseFloat(capacity[i])
     maxIndex = i
     }
   }
-  if(stadium[maxIndex] == 0){
+  if(maxIndex == 0){
     return "False division"
   }
   return stadium[maxIndex];
 }
-
-console.log(findMaxCapactiy("East"));
-
-
-//Putting in the division and conference and get the averageCapacity of the stadium in the stadium 
-//sum - is a parameter = 0, for being the total capacity of the stadiums
-//stadiums - a list contained with all the stdiums  
-//divisons - a list contained with divisions 
-//conferences - a list containing all the conferences
-//returns the sum of the stadium and divide by how many stadium are there
+console.log(findMaxCapactiy("Afc east"));
+console.log(findMaxCapactiy("afc west"));
+console.log(findMaxCapactiy("north"));
+console.log(findMaxCapactiy("afc"));
+console.log(findMaxCapactiy("NFC"));
+console.log(findMaxCapactiy("West"));
+console.log(findMaxCapactiy("Atlantic"));
 
 
-function averageCapacity(division){
+/*
+Uses an NFL division as a parameter and returns the average capacity of the stadiums in the divison
+@param division(string) - the user's desired NFL division
+@return sum/stadiums.length(number) - returns the average stadium capacity of the entered division
+*/
+
+
+function getAverageCapacity(division){
   var sum = 0;
   var stadiums = []
   for (var i in capacity){
-    if(conferences[i].toLowerCase() + " " + divisions[i].toLowerCase() == division.toLowerCase()){
+    if((conferences[i].toLowerCase() + " " + divisions[i].toLowerCase()).includes(division.toLowerCase())){
       sum +=(parseFloat(capacity[i].substring(1, capacity[i].length - 1)));
       stadiums.push(stadium[i]);
     }
@@ -125,18 +136,23 @@ function averageCapacity(division){
   return sum / stadiums.length
 }
 
-console.log(averageCapacity("AFC North"));
+console.log(averageCapacity("Afc east"));
+console.log(averageCapacity("afc west"));
+console.log(averageCapacity("north"));
+console.log(averageCapacity("afc"));
+console.log(averageCapacity("NFC"));
+console.log(averageCapacity("West"));
+console.log(averageCapacity("Atlantic"));
 
-//How many teams are there in each division
-//divisons - a list contained with divisions 
-//conferences - a list containing all the conferences
-//returns the count list which is the amount teams in each divsion
-//count is a list created for storing the result of the function
-
+/*
+Uses an NFL division as a parameter and returns the number of teams in the entered division
+@param division(string) - the user's desired NFL division
+@return count.length(number) - returns the total number of teams in the entered NFL division
+*/
 function getAppear(division){
   var count = [];
   for (var i =  0; i < divisions.length; i++){
-    if(division.toLowerCase() ==conferences[i].toLowerCase() + " " + divisions[i].toLowerCase()){
+    if((conferences[i].toLowerCase() + " " + divisions[i].toLowerCase()).includes(division.toLowerCase())){
        count.push(teams[i])
     }
   }
@@ -146,9 +162,10 @@ function getAppear(division){
   return count.length
 }
 
-console.log(getAppear("AFC North"));
-
-
-
-
-
+console.log(getAppear("Afc east"));
+console.log(getAppear("afc west"));
+console.log(getAppear("north"));
+console.log(getAppear("afc"));
+console.log(getAppear("NFC"));
+console.log(getAppear("West"));
+console.log(getAppear("Atlantic"));
