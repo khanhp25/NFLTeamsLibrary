14a6575b36fea31327ad746a4176a6b70664240a
khanhp25 <116367383+khanhp25@users.noreply.github.com>
2023-04-28 23:34:41 -0400

    Update NFL Teams.js

--- NFL Teams.js
+++ NFL Teams.js
@@ -33,6 +33,7 @@ var stadium = getColumn(url, 5);
 var capacity = getColumn(url, 6);
 var headCoach = getColumn(url, 7);
 
+console.log(capacity);
 
 /*
 Takes an NFL team as a parameter and uses it to print out the head coach of the entered team
@@ -52,13 +53,6 @@ function findCoach(team){
   return coach;
 }
 
-console.log(findCoach("New Orleans Pelicans"));
-console.log(findCoach("buffalo bills"));
-console.log(findCoach("new england"))
-console.log(findCoach("giants"));
-console.log(findCoach("baltimore"));
-console.log(findCoach("cinci"));
-console.log(findCoach("Browns"));
 /*
 Uses an NFL division as a parameter and returns all of the stadiums in the entered division
 @param division(string) - the user's desired NFL division
@@ -76,42 +70,31 @@ function getStadiumsInDivision(division){
   }
   return matches;
 }
-console.log(getStadiumsInDivision("Afc east"));
-console.log(getStadiumsInDivision("afc west"));
-console.log(getStadiumsInDivision("north"));
-console.log(getStadiumsInDivision("afc"));
-console.log(getStadiumsInDivision("NFC"));
-console.log(getStadiumsInDivision("West"));
-console.log(getStadiumsInDivision("Atlantic"));
+
 
 
 /*
-Uses an NFL division as a parameter and returns the stadium in the division with the highest capacity
+Uses an NFL division as a parameter and returns the stadium in the division with the lowest capacity
 @param division(string) - the user's desired NFL division
 @return stadium[maxIndex](string) - returns the stadium with the largest capacity in the entered division
 */
 
-function findMaxCapactiy(division){
-  max = 10000000;
-  maxIndex = 0;
+function findLowestCapactiy(division){
+  max = 82500;
+  minIndex = 0;
   for(var i in divisions){
     if((conferences[i].toLowerCase() + " " + divisions[i].toLowerCase()).includes(division.toLowerCase()) && parseFloat(capacity[i]) < max){
       max = parseFloat(capacity[i])
-    maxIndex = i
+    minIndex = i
     }
   }
-  if(maxIndex == 0){
+  if(minIndex == 0){
     return "False division"
   }
-  return stadium[maxIndex];
+  return stadium[minIndex];
 }
-console.log(findMaxCapactiy("Afc east"));
-console.log(findMaxCapactiy("afc west"));
-console.log(findMaxCapactiy("north"));
-console.log(findMaxCapactiy("afc"));
-console.log(findMaxCapactiy("NFC"));
-console.log(findMaxCapactiy("West"));
-console.log(findMaxCapactiy("Atlantic"));
+
+
 
 
 /*
@@ -126,30 +109,25 @@ function getAverageCapacity(division){
   var stadiums = []
   for (var i in capacity){
     if((conferences[i].toLowerCase() + " " + divisions[i].toLowerCase()).includes(division.toLowerCase())){
-      sum +=(parseFloat(capacity[i].substring(1, capacity[i].length - 1)));
+      sum +=(parseFloat(capacity[i]));
       stadiums.push(stadium[i]);
     }
   }
+  
   if (stadiums.length == 0){
     return "False division";
   }
   return sum / stadiums.length
 }
 
-console.log(averageCapacity("Afc east"));
-console.log(averageCapacity("afc west"));
-console.log(averageCapacity("north"));
-console.log(averageCapacity("afc"));
-console.log(averageCapacity("NFC"));
-console.log(averageCapacity("West"));
-console.log(averageCapacity("Atlantic"));
+
 
 /*
 Uses an NFL division as a parameter and returns the number of teams in the entered division
 @param division(string) - the user's desired NFL division
 @return count.length(number) - returns the total number of teams in the entered NFL division
 */
-function getAppear(division){
+function getTotalStadiums(division){
   var count = [];
   for (var i =  0; i < divisions.length; i++){
     if((conferences[i].toLowerCase() + " " + divisions[i].toLowerCase()).includes(division.toLowerCase())){
@@ -162,10 +140,3 @@ function getAppear(division){
   return count.length
 }
 
-console.log(getAppear("Afc east"));
-console.log(getAppear("afc west"));
-console.log(getAppear("north"));
-console.log(getAppear("afc"));
-console.log(getAppear("NFC"));
-console.log(getAppear("West"));
-console.log(getAppear("Atlantic"));
